370c009256fc2cc7ee0b2661a47cdb33eb914584
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-08 08:11:12 +0530

    student management system

--- studentMgmtTeam6/src/main/java/dxc/b2/t6/StudentMgmtTeam6Application.java
+++ studentMgmtTeam6/src/main/java/dxc/b2/t6/StudentMgmtTeam6Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentMgmtT6Application {
+public class StudentMgmtTeam6Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentMgmtT6Application.class, args);
+		SpringApplication.run(StudentMgmtTeam6Application.class, args);
 	}
 
 }
--- studentMgmtTeam6/src/test/java/dxc/b2/t6/StudentMgmtTeam6ApplicationTests.java
+++ studentMgmtTeam6/src/test/java/dxc/b2/t6/StudentMgmtTeam6ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StudentMgmtT6ApplicationTests {
+class StudentMgmtTeam6ApplicationTests {
 
 	@Test
 	void contextLoads() {
